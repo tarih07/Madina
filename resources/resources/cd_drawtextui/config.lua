784f375312fecd6e2b881f91da4e2418ffbb90e7
tarih07 <116463002+tarih07@users.noreply.github.com>
2023-01-18 05:22:48 +0100

    Add files via upload

---
+++ resources/resources/cd_drawtextui/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+Config.Locations = {
+	[1] = {
+		coords = vector3(462.6101, -1019.65, 28.105),
+		distance = 4,
+		key = 38,
+		eventname = 'event:control:bennys',
+		text = '<b>[E] Police Workshop</b>'
+	},
+	
+}
