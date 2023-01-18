44eefc1c53669b29f7d98d09ea1de649b8f14ea8
tarih07 <116463002+tarih07@users.noreply.github.com>
2023-01-18 14:33:56 +0100

    Add files via upload

---
+++ resources/[truck]/dafeuro6/__resource.lua
@@ -0,0 +1,25 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+-- Manifest
+
+files {
+	--'data/carcols.meta',
+	'data/carvariations.meta',
+	'data/handling.meta',
+	'data/vehicles.meta',
+    'data/caraddoncontentunlocks.meta',
+    --'data/vehiclelayouts.meta',
+   -- 'data/shop_vehicle.meta',
+}
+
+
+--data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/caraddoncontentunlocks.meta'
+--data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+--data_file 'VEHICLE_SHOP_DLC_FILE' 'data/shop_vehicle.meta'
+
+
+--client_script { 'client/vehicle_name.lua' }
